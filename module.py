875094d0d8bf78845d1761aa3c9489ce24a85792
Youk ChangSu <csyouk@gmail.com>
2019-11-10 19:52:24 +0900

    init
    Signed-off-by: Youk ChangSu <csyouk@gmail.com>

---
+++ module.py
@@ -0,0 +1,14 @@
+def hello_world():
+    return 'Hell world'
+
+
+def name():
+    return 'youk'
+
+
+def main():
+    print(hello_world())
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_module.py
@@ -0,0 +1,10 @@
+from module import hello_world
+from module import name
+
+
+def test_hello_world():
+    assert 'Hell world' == hello_world()
+
+
+def test_name():
+    assert 'youk' == name()
